309b60c916c07df7663600ade0f1343fb8c5889d
ROHON ALAM <ra.official.9775@gmail.com>
2026-07-11 09:12:22 +0530

    Time: 3 ms (85.18%) | Memory: 21.1 MB (49.68%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,6 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        #BRUTE FORCE
+        # BRUTE FORCE
         # n = len(nums)
         # for n1 in nums:
         #     freq = 0
@@ -10,14 +10,43 @@ class Solution:
         #     if(freq >= n/2):
         #         return n1
 
-        n = len(nums)
-        m = defaultdict(int)
+
+        #Using defaultdict
+        # n = len(nums)
+        # m = defaultdict(int)
+        # for num in nums:
+        #     m[num] += 1
+
+        # for key, value in m.items():
+        #     if value >= n / 2:
+        #         return key
+
+        #solving in O(1) space complexity
+        #Moore's voting algorithm
+
+        candidate = None
+        count = 0
+
         for num in nums:
-            m[num] += 1
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        return candidate
 
-        for key,value in m.items():
-            if value >= n/2:
-                return key
 
 
-            
+
+
+
+
+
+
+
+
+
+
+
+
