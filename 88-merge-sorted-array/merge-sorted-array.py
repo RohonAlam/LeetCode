492fb0f5a9bd0d865456e9a2af7c3a1bdade27e8
ROHON ALAM <ra.official.9775@gmail.com>
2026-07-11 10:03:25 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (95.57%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -0,0 +1,48 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        #Brute Force
+        # nums1[m:] = nums2
+        # nums1.sort()
+
+        #Using Extra Array(Two pointer/mergesort)
+        # res = []
+        # i=j = 0
+
+        # while i <m and j <n :
+        #     if nums1[i]<=nums2[j]:
+        #         res.append(nums1[i])
+        #         i += 1
+        #     else:
+        #         res.append(nums2[j])
+        #         j += 1
+        # while i<m:
+        #     res.append(nums1[i])
+        #     i+=1
+        # while j<n:
+        #     res.append(nums2[j])
+        #     j+=1
+
+        # nums1[:] = res
+
+
+        #Using Three pointers from back
+        i = m-1
+        j = n-1
+        idx = m+n-1
+        
+        while(i>=0 and j >=0):
+            if(nums1[i]>=nums2[j]):
+                nums1[idx] = nums1[i]
+                i -= 1
+                idx -=1
+            else:
+                nums1[idx] = nums2[j]
+                j -= 1
+                idx -= 1
+        while(j>=0):
+            nums1[idx] = nums2[j]
+            j -= 1 
+            idx -= 1
