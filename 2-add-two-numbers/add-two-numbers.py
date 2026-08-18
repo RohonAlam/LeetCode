89540639c68fc39e9e8680c96f2b0a3a70d4be4a
ROHON ALAM <ra.official.9775@gmail.com>
2026-08-19 00:06:29 +0530

    Time: 7 ms (24.13%) | Memory: 19.3 MB (41.22%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,6 +5,9 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        #Approach1 (converting to int and them sum and again create linkedlist)
+        """
         curr1 = l1
         num1 = 0
         curr2 =l2
@@ -38,6 +41,36 @@ class Solution:
 
 
         return head
-        
+"""
+        #Approach2 (directly sum using carry)
 
-        
+        dummy = ListNode(0)
+        current = dummy
+
+        carry = 0
+
+        while l1 or l2 or carry:
+
+            # Get current digits
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            # Add digits + carry
+            total = val1 + val2 + carry
+
+            # Calculate digit and carry
+            digit = total % 10
+            carry = total // 10
+
+            # Create new node
+            current.next = ListNode(digit)
+            current = current.next
+
+            # Move pointers if possible
+            if l1:
+                l1 = l1.next
+
+            if l2:
+                l2 = l2.next
+
+        return dummy.next
