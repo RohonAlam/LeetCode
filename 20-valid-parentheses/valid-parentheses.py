e82090b189932b806eca258b9acf00b04f836ba2
ROHON ALAM <ra.official.9775@gmail.com>
2026-08-27 00:45:37 +0530

    Time: 2 ms (37.08%) | Memory: 19.3 MB (63.22%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,5 +1,6 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        """
         stack = []
 
         for ch in s :
@@ -15,4 +16,27 @@ class Solution:
                 else:
                     stack.append(ch)
         return len(stack)==0
-        
+"""
+    #Better logics and return false with first condition break
+        stack = []
+
+        for ch in s:
+            if ch in "([{":
+                stack.append(ch)
+
+            else:
+                if not stack:
+                    return False
+
+                elif ch == ')' and stack[-1] != '(':
+                    return False
+
+                elif ch == '}' and stack[-1] != '{':
+                    return False
+
+                elif ch == ']' and stack[-1] != '[':
+                    return False
+
+                stack.pop()
+
+        return len(stack) == 0       
