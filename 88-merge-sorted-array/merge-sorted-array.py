99bd47c331405de52c7d6441990b17a6f3f4b915
ROHON ALAM <ra.official.9775@gmail.com>
2026-09-21 12:39:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (95.83%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,48 +1,20 @@
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        #Brute Force
-        # nums1[m:] = nums2
-        # nums1.sort()
+        p = m-1
+        q = n-1
+        ptr = m+n-1
 
-        #Using Extra Array(Two pointer/mergesort)
-        # res = []
-        # i=j = 0
-
-        # while i <m and j <n :
-        #     if nums1[i]<=nums2[j]:
-        #         res.append(nums1[i])
-        #         i += 1
-        #     else:
-        #         res.append(nums2[j])
-        #         j += 1
-        # while i<m:
-        #     res.append(nums1[i])
-        #     i+=1
-        # while j<n:
-        #     res.append(nums2[j])
-        #     j+=1
-
-        # nums1[:] = res
-
-
-        #Using Three pointers from back
-        i = m-1
-        j = n-1
-        idx = m+n-1
-        
-        while(i>=0 and j >=0):
-            if(nums1[i]>=nums2[j]):
-                nums1[idx] = nums1[i]
-                i -= 1
-                idx -=1
+        while q >= 0:
+            if p >= 0 and nums1[p] > nums2[q]:
+                nums1[ptr] = nums1[p]
+                p -= 1
             else:
-                nums1[idx] = nums2[j]
-                j -= 1
-                idx -= 1
-        while(j>=0):
-            nums1[idx] = nums2[j]
-            j -= 1 
-            idx -= 1
+                nums1[ptr] = nums2[q]
+                q -= 1
+
+            ptr -= 1
+
+        
